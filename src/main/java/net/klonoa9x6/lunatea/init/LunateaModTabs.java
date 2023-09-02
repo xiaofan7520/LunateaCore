ada3a328ef0bb14a46cc552a2d621842516dd629
KLONOA9X6 <daliu2004@outlook.com>
2023-09-02 17:15:58 +0800

    唱片

--- src/main/java/net/klonoa9x6/lunatea/init/LunateaModTabs.java
+++ src/main/java/net/klonoa9x6/lunatea/init/LunateaModTabs.java
@@ -18,12 +18,13 @@ public class LunateaModTabs {
 	public static final DeferredRegister<CreativeModeTab> REGISTRY = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, LunateaMod.MODID);
 	public static final RegistryObject<CreativeModeTab> LUNATEA_GROUP = REGISTRY.register("lunatea_group",
 			() -> CreativeModeTab.builder().title(Component.translatable("item_group.lunatea.lunatea_group")).icon(() -> new ItemStack(LunateaModItems.WIND_RING.get())).displayItems((parameters, tabData) -> {
+				tabData.accept(LunateaModItems.WIND_RING.get());
 				tabData.accept(LunateaModItems.DISC_UNDERTALE.get());
 				tabData.accept(LunateaModItems.DISC_MOONSONG.get());
 				tabData.accept(LunateaModItems.DISC_NOTE_BLOCK_BATTLE.get());
 				tabData.accept(LunateaModItems.DISC_WIND_GARDEN_GALAXY.get());
 				tabData.accept(LunateaModItems.DISC_MAGIC_SPEAR_I.get());
-				tabData.accept(LunateaModItems.WIND_RING.get());
+				tabData.accept(LunateaModItems.DISC_PRIAMILL_KLONOA_G_1.get());
 			})
 
 					.build());
