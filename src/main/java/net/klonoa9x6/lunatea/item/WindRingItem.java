7372b03652e6c92d39b23b610ba7b3bb8d2fbeb2
KLONOA9X6 <daliu2004@outlook.com>
2023-09-03 09:53:43 +0800

    初步逻辑

--- src/main/java/net/klonoa9x6/lunatea/item/WindRingItem.java
+++ src/main/java/net/klonoa9x6/lunatea/item/WindRingItem.java
@@ -1,18 +1,35 @@
 
 package net.klonoa9x6.lunatea.item;
 
+import net.minecraft.world.level.Level;
 import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.InteractionHand;
+
+import net.klonoa9x6.lunatea.procedures.WindRingFunctionProcedure;
 
 public class WindRingItem extends Item {
 	public WindRingItem() {
-		super(new Item.Properties().stacksTo(64).rarity(Rarity.EPIC));
+		super(new Item.Properties().stacksTo(1).rarity(Rarity.EPIC));
 	}
 
 	@Override
 	public UseAnim getUseAnimation(ItemStack itemstack) {
 		return UseAnim.CROSSBOW;
+	}
+
+	@Override
+	public InteractionResultHolder<ItemStack> use(Level world, Player entity, InteractionHand hand) {
+		InteractionResultHolder<ItemStack> ar = super.use(world, entity, hand);
+		ItemStack itemstack = ar.getObject();
+		double x = entity.getX();
+		double y = entity.getY();
+		double z = entity.getZ();
+		WindRingFunctionProcedure.execute(world, entity, itemstack);
+		return ar;
 	}
 }
---
+++ src/main/java/net/klonoa9x6/lunatea/procedures/WindRingFunctionProcedure.java
@@ -0,0 +1,33 @@
+package net.klonoa9x6.lunatea.procedures;
+
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.damagesource.DamageTypes;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.core.registries.Registries;
+
+public class WindRingFunctionProcedure {
+	public static void execute(LevelAccessor world, Entity entity, ItemStack itemstack) {
+		if (entity == null)
+			return;
+		if (entity instanceof Player _playerHasItem ? _playerHasItem.getInventory().contains(new ItemStack(Items.SPECTRAL_ARROW)) : false) {
+			if ((entity instanceof Player _plr ? _plr.experienceLevel : 0) >= 3) {
+				if (entity instanceof Player _player) {
+					ItemStack _stktoremove = new ItemStack(Items.SPECTRAL_ARROW);
+					_player.getInventory().clearOrCountMatchingItems(p -> _stktoremove.getItem() == p.getItem(), 1, _player.inventoryMenu.getCraftSlots());
+				}
+			} else {
+				if (entity instanceof Player _player) {
+					ItemStack _stktoremove = new ItemStack(Items.SPECTRAL_ARROW);
+					_player.getInventory().clearOrCountMatchingItems(p -> _stktoremove.getItem() == p.getItem(), 1, _player.inventoryMenu.getCraftSlots());
+				}
+				entity.hurt(new DamageSource(world.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(DamageTypes.MAGIC)), 5);
+			}
+			if (entity instanceof Player _player)
+				_player.getCooldowns().addCooldown(itemstack.getItem(), 60);
+		}
+	}
+}
